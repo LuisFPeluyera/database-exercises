9becd89c7588c6a7106b9d916633b7395a9bb6a9
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-10-23 12:24:32 -0500

    tables exercise complete

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,8 +6,8 @@ drop table if exists albums;
 
 CREATE TABLE albums (
                         id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT,
-                        artist CHAR(50) DEFAULT 'NONE',
-                        name  CHAR(50) NOT NULL,
+                        artist VARCHAR(50) DEFAULT 'NONE',
+                        name  VARCHAR(50) NOT NULL,
                         release_date INT UNSIGNED NOT NULL,
                         sales decimal(3,1) NOT NULL,
                         genre CHAR(50) NOT NULL,
