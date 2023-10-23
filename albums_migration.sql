c505b40a44bdcdcd72e27981c23fe2d2af2c4be4
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-10-23 11:59:31 -0500

    tables-exercises init

---
+++ albums_migration.sql
@@ -0,0 +1,15 @@
+# recreate my albums table
+# switch to db first
+use codeup_test_db;
+
+drop table if exists albums;
+
+CREATE TABLE albums (
+                        id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist CHAR(50) DEFAULT 'NONE',
+                        name  CHAR(50) NOT NULL,
+                        release_date INT UNSIGNED NOT NULL,
+                        sales decimal(3,1) NOT NULL,
+                        genre CHAR(50) NOT NULL,
+                        PRIMARY KEY (id)
+);
